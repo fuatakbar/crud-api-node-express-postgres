f511534ca208d413866e69edc8a1d84aaeff3d94
Fuat Akbar <fuatakbars@gmail.com>
2021-02-09 01:38:11 +0700

    Add create, update and delete function

--- queries.js
+++ queries.js
@@ -26,3 +26,37 @@ const getUserById = (req, res) => {
         res.status(200).json(results.rows)
     })
 }
+
+const createUser = (req, res) => {
+    const { name, email } = req.body
+    pool.query('UPDATE users SET name = $name, email = $email', [name, email], (error, results) => {
+        if (error) {
+            throw error
+        }
+
+        res.status(201).send(`User Created Successfully with ID: ${results.insertId}`)
+    })
+}
+
+const updateUser = (req, res) => {
+    const id = parseInt(req.params.id)
+    const { name, email } = req.body
+    pool.query('INSERT INTO users (name, email) VALUES ($name, $email) WHERE id = $id', [name, email, id], (error, results) => {
+        if (error) {
+            throw error
+        }
+
+        res.status(200).send(`User Updated with ID: ${id}`)
+    })
+}
+
+const deleteUser = (req, res) => {
+    const id = req.params.id
+    pool.query('DELETE users WHERE id = $id', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+
+        res.status(200).send(`User Has Been Deleted with ID: ${id}`)
+    })
+}
