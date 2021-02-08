3f57ec55f26a113b8f72e0dd4362cb571cb145e9
Fuat Akbar <fuatakbars@gmail.com>
2021-02-09 01:08:04 +0700

    Add get user function response

--- queries.js
+++ queries.js
@@ -6,3 +6,13 @@ const pool = new Pool({
     password: 'password',
     port: '5432',
 })
+
+const getUsers = (req, res) => {
+    pool.query('SELECT * FROM users', (error, results) => {
+        if (error) {
+            throw error
+        }
+
+        res.status(200).json(results.rows)
+    })
+}
